45ef9fbbee0a8e793601a9ee6c505bc7bfe002e0
thoomaas23 <thomas.lazar02@e-uvt.ro>
2023-05-18 13:07:55 +0300

    Update 1.3

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 let currentDate = `${day}.${month}.${year}`;
-const nota = ' Notă 
--	Primitorul are obligația să asigure paza și depozitarea în bune condiții a bunurilor primite în subgestiune;
--	Primitorul are obligația să anunțe gestionarul în cazul constatării apariției defecțiunilor, sau în cazul pierderii sau furtului, în maxim 24 de ore de la constatarea incidentului;
-  Primitorul are obligația de a pune la dispoziția proprietarului/gestinarului bunurile aflate în subgestiune la orice solicitare a acestuia învederea verificării stării tehnice sau a inventarierii.' ; 
+const nota_1 = 'Primitorul are obligația să asigure paza și depozitarea în bune condiții a bunurilor primite în subgestiune;';
+const nota_2 = 'Primitorul are obligația să anunțe gestionarul în cazul constatării apariției defecțiunilor, sau în cazul pierderii sau furtului, în maxim 24 de ore de la constatarea incidentului;';
+const nota_3 = 'Primitorul are obligația de a pune la dispoziția proprietarului/gestinarului bunurile aflate în subgestiune la orice solicitare a acestuia învederea verificării stării tehnice sau a inventarierii.';
 // Pipe its output somewhere, like to a file or HTTP response
 // See below for browser usage
 doc.pipe(fs.createWriteStream('output.pdf'));
@@ -35,6 +34,6 @@ doc.font('Times-Italic')
     .text('avand ca obiect folosinta mijloacelor fixe/obiectelor de inventar' , {  width: 410,align: 'center'});
 doc.moveDown(2);
 doc.font('Times-Roman')
-    .text(`This text is left aligned. ${nota}`);
+    .text(`This text is left aligned. ${}`);
 
 doc.end();
