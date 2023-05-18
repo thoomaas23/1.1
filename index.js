a47facc375eafed6891093f5065c55afe3918142
thoomaas23 <thomas.lazar02@e-uvt.ro>
2023-05-18 13:19:36 +0300

    Update 1.3

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const date = new Date();
 let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
-let currentDate = `${day}.${month}.${year}`;
+let currentDate ; 
+if (month >= 10 ){currentDate = `${day}.${month}.${year}`;}
+else {currentDate = `${day}.0${month}.${year}`;}
+const data_incheiat_gestionare  = `Încheiat astazi ${currentDate}.`;
 const nota_1 = 'Primitorul are obligația să asigure paza și depozitarea în bune condiții a bunurilor primite în subgestiune;';
 const nota_2 = 'Primitorul are obligația să anunțe gestionarul în cazul constatării apariției defecțiunilor, sau în cazul pierderii sau furtului, în maxim 24 de ore de la constatarea incidentului;';
 const nota_3 = 'Primitorul are obligația de a pune la dispoziția proprietarului/gestinarului bunurile aflate în subgestiune la orice solicitare a acestuia învederea verificării stării tehnice sau a inventarierii.';
@@ -34,6 +37,6 @@ doc.font('Times-Italic')
     .text('avand ca obiect folosinta mijloacelor fixe/obiectelor de inventar' , {  width: 410,align: 'center'});
 doc.moveDown(2);
 doc.font('Times-Roman')
-    .text(`This text is left aligned. ${}`);
+    .text(`This text is left aligned. ${data_incheiat_gestionare}`);
 
 doc.end();
